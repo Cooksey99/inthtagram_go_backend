942fb23838b0fdd6473e6e3f3350635353adfe2d
cooksey <andrewcooksey42@gmail.com>
2022-05-05 18:15:20 -0500

    removed irrelevant files, added packages, and added a test route to main.go

--- backend/main.go
+++ backend/main.go
@@ -1,7 +1,20 @@
 package main
 
-import "backend/config"
+import (
+	"backend/config"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	// "backend/routes"
+)
 
 func main() {
-	config.ConnectToDB();
+	config.ConnectToDB()
+	r := gin.New()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	r.Run()
 }
